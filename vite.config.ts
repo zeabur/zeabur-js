7dbe16ff23baccd262a94ff2a7b6e69debcfbe64
michaelyuhe <2312744987@qq.com>
2024-06-25 13:38:13 +0800

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
-import packageJson from "./package.json";
 
-const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
 
 const fileName = {
-  es: `${getPackageName()}.mjs`,
-  cjs: `${getPackageName()}.cjs`,
-  iife: `${getPackageName()}.iife.js`,
+  es: 'index.mjs',
+  cjs: 'index.cjs',
+  iife: 'index.iife.js',
 };
 
 const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
@@ -27,7 +15,7 @@ module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
+      name: 'Zeabur',
       formats,
       fileName: (format) => fileName[format],
     },
